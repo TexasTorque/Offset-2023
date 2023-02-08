82ae3d2f610e0134588df39abcd16b0fc9c0652b
Juicestus <jus@gtsbr.org>
2023-02-08 10:32:23 -0600

    Working on rotation problem

--- src/main/java/org/texastorque/subsystems/Arm.java
+++ src/main/java/org/texastorque/subsystems/Arm.java
@@ -22,6 +22,7 @@ import com.ctre.phoenix.sensors.SensorTimeBase;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import io.github.oblarg.oblog.annotations.Config;
 import io.github.oblarg.oblog.annotations.Log;
@@ -30,27 +31,34 @@ public final class Arm extends TorqueSubsystem implements Subsystems {
     public static class ArmPose {
         private static final double ELEVATOR_TOLERANCE = 0.1, ROTARY_TOLERANCE = 0.1;
 
-        public final double elevatorPose, rotaryPose;
+        public final double elevatorPose;
+        public final Rotation2d rotaryPose;
 
-        public ArmPose(final double elevatorPose, final double rotaryPose) {
+        public ArmPose(final double elevatorPose, final Rotation2d rotaryPose) {
             this.elevatorPose = elevatorPose;
             this.rotaryPose = rotaryPose;
         }
 
-        public boolean atPose(final double elevatorReal, final double rotaryReal) {
-            return Math.abs(elevatorReal - elevatorPose) < ELEVATOR_TOLERANCE && Math.abs(rotaryReal - rotaryPose) < ROTARY_TOLERANCE;
+        public boolean atPose(final double elevatorReal, final Rotation2d rotaryReal) {
+            return Math.abs(elevatorReal - elevatorPose) < ELEVATOR_TOLERANCE 
+                    && Math.abs(rotaryReal.minus(rotaryPose).getRadians()) < ROTARY_TOLERANCE;
         }
     }
 
     public static enum State {
-        HANDOFF(new ArmPose(0, 0)),
+        HANDOFF(new ArmPose(0, Rotation2d.fromRadians(0))),
         // Position to grab from indexer (contacts indexer)
-        DOWN(new ArmPose(0, 0)),
+        DOWN(new ArmPose(0, Rotation2d.fromRadians(0))),
         // Position to grab from ground (contacts ground)
-        SHELF(new ArmPose(.5, Math.PI)),                  // Position to grab from shelf (contacts shelf)
-        MID(new ArmPose(.5, 0), new ArmPose(0, 0)), // Position to grab from human player (contacts
-        // human player)
-        TOP(new ArmPose(.5, 0), new ArmPose(0, 0)); // Position to intake (contacts intake)
+        SHELF(new ArmPose(.5, Rotation2d.fromRadians(Math.PI))),                  // Position to grab from shelf (contacts shelf)
+        MID(
+                new ArmPose(.5, Rotation2d.fromRadians(0)), 
+                new ArmPose(.5, Rotation2d.fromRadians(0))
+        ), // Position to grab from human player (contacts human player)
+        TOP(
+                new ArmPose(.5,  Rotation2d.fromRadians(0)), 
+                new ArmPose(0,  Rotation2d.fromRadians(0))
+        ); // Position to intake (contacts intake)
 
         public final ArmPose cubePose;
         public final ArmPose conePose;
@@ -65,30 +73,44 @@ public final class Arm extends TorqueSubsystem implements Subsystems {
         public ArmPose get() { return hand.getGamePieceMode() == GamePiece.CUBE ? cubePose : conePose; }
     }
 
-    private static volatile Arm instance;
-
-    private static final double ARM_ROTARY_ENCODER_OFFSET = 4.279797747731209, ELEVATOR_MOTOR_ROT_PER_METER = 1 / 37.5956558484, ELEVATOR_MAX_VOLTS = 12, ROTARY_MAX_VOLTS = 12, ELEVATOR_MIN = 0, ELEVATOR_MAX = .546;
+    private static final double ARM_ROTARY_ENCODER_OFFSET = 4.279797747731209, 
+            ELEVATOR_MOTOR_ROT_PER_METER = 1 / 37.5956558484, 
+            ELEVATOR_MAX_VOLTS = 12, 
+            ROTARY_MAX_VOLTS = 12, 
+            ELEVATOR_MIN = 0, 
+            ELEVATOR_MAX = .546;
 
     public static final double ARM_INTERFERE_MIN = (5. / 6.) * Math.PI;
 
     public static final double ARM_INTERFERE_MAX = (11. / 6.) * Math.PI;
+    public static final double TWO_PI_RAD = Math.PI * 2;
 
+    public static final Rotation2d TWO_PI_R2D = Rotation2d.fromRadians(TWO_PI_RAD);
+
+    private static volatile Arm instance;
     public static final synchronized Arm getInstance() { return instance == null ? instance = new Arm() : instance; }
+
+    public static Rotation2d constrain(final Rotation2d theta) {
+        return Rotation2d.fromRadians((theta.getRadians() + 2 * Math.PI) % (2 * Math.PI));
+    }
+
+    public static Rotation2d reverse(final Rotation2d theta) {
+        return Rotation2d.fromRadians(2 * Math.PI - theta.getRadians());
+    }
 
     @Log.ToString
     private State activeState = State.HANDOFF;
-
     @Log.ToString
     private State desiredState = State.HANDOFF;
-
     @Log.ToString
     public double realElevatorPose = 0;
-    @Log.ToString
-    public double realRotaryPose = 0;
-    private final TorqueNEO elevator = new TorqueNEO(Ports.ARM_ELEVATOR_MOTOR);
 
+    @Log.ToString
+    public Rotation2d realRotaryPose = Rotation2d.fromRadians(0);
+    private final TorqueNEO elevator = new TorqueNEO(Ports.ARM_ELEVATOR_MOTOR);
     @Config
     public final PIDController elevatorPoseController = new PIDController(20, 0, 0);
+
     // Not using rn
     private final ElevatorFeedforward elevatorPoseFeedForward = new ElevatorFeedforward(0, .45, 4.6);
     // new ElevatorFeedforward(1.01, 6.14, 0.16);
@@ -104,12 +126,12 @@ public final class Arm extends TorqueSubsystem implements Subsystems {
 
     @Config
     public final PIDController rotaryPoseController = new PIDController(1, 0, 0);
+
     // Unfortunatly these are not sendables
     // Est. from https://www.reca.lc/arm
     public final ArmFeedforward rotaryPoseFeedForward = new ArmFeedforward(0, 1.6, .5);
 
     private final CANCoder rotaryEncoder = new CANCoder(Ports.ARM_ROTARY_ENCODER);
-
 
     @Log.BooleanBox
     private boolean wantsHandoff = false;
@@ -118,7 +140,7 @@ public final class Arm extends TorqueSubsystem implements Subsystems {
         rotaryPoseController.enableContinuousInput(0, (3./2.) * Math.PI);
 
         elevator.setPositionConversionFactor(ELEVATOR_MOTOR_ROT_PER_METER);
-        elevator.setCurrentLimit(60);
+        elevator.setCurrentLimit(20);
         elevator.setVoltageCompensation(12.6);
         elevator.setBreakMode(true);
         elevator.burnFlash();
@@ -147,28 +169,50 @@ public final class Arm extends TorqueSubsystem implements Subsystems {
     public boolean isAtScoringPose() {
         return activeState == State.MID || activeState == State.TOP;
     }
-
     public void setState(final State state) { this.desiredState = state; }
 
     public State getState() { return desiredState; }
     public boolean isState(final State state) { return getState() == state; }
 
+
     @Override
     public final void initialize(final TorqueMode mode) {}
+
     public boolean isAtDesiredPose() { return activeState.get().atPose(realElevatorPose, realRotaryPose); }
 
     @Override
     public final void update(final TorqueMode mode) {
         activeState = desiredState;
-
         wantsHandoff = activeState == State.HANDOFF;
         if (wantsHandoff && indexer.isConflictingWithArm()) activeState = State.DOWN;
 
-        realElevatorPose = elevator.getPosition();
-        realRotaryPose = ((rotaryEncoder.getPosition() + (2 * Math.PI)) % (2 * Math.PI) ) - ARM_ROTARY_ENCODER_OFFSET;
-        realRotaryPose = -((realRotaryPose + (2 * Math.PI)) % (2 * Math.PI) );
-        realRotaryPose = ((realRotaryPose + (2 * Math.PI)) % (2 * Math.PI) );
+        updateFeedback();
 
+        calculateElevator(false);
+        calculateRotary(false);
+    }
+
+    @Log.BooleanBox
+    public final boolean isConflictingWithIndexer() {
+        // return ARM_INTERFERE_MIN < realRotaryPose && realRotaryPose < ARM_INTERFERE_MAX;
+        return false;
+    }
+
+    @Log.BooleanBox
+    public final boolean wantsToConflictWithIndexer() {
+        return wantsHandoff;
+    }
+
+    private Rotation2d readRotaryPose() {
+        return constrain(Rotation2d.fromRadians(rotaryEncoder.getPosition() - ARM_ROTARY_ENCODER_OFFSET)).unaryMinus();
+    }
+
+    private void updateFeedback() {
+        realElevatorPose = elevator.getPosition();
+        realRotaryPose = Rotation2d.fromRadians(rotaryEncoder.getPosition());
+    }
+
+    private void calculateElevator(final boolean output) {
         final double elevatorPIDOutput = elevatorPoseController.calculate(realElevatorPose, activeState.get().elevatorPose);
         SmartDashboard.putNumber("arm::elevatorPIDOutput", elevatorPIDOutput);
 
@@ -181,28 +225,20 @@ public final class Arm extends TorqueSubsystem implements Subsystems {
         final double constrainedElevatorVolts = TorqueMath.linearConstraint(requestedElevatorVolts, realElevatorPose, ELEVATOR_MIN, ELEVATOR_MAX); // don't think this will work
         SmartDashboard.putNumber("arm::constrainedElevatorVolts", constrainedElevatorVolts);
 
-        // elevator.setVolts(constrainedElevatorVolts);
+        elevator.setVolts(output ? constrainedElevatorVolts : 0);
+    }
 
-        final double adjSetpoint = activeState.get().rotaryPose;
+    private void calculateRotary(final boolean output) {
+        final double adjSetpoint = 0;//activeState.get().rotaryPose;
         SmartDashboard.putNumber("arm::adjSetpoint", adjSetpoint);
 
         final double rotaryFFOutput = -rotaryPoseFeedForward.calculate(adjSetpoint, 0);
 
-        final double rotarayPIDDOutput = rotaryPoseController.calculate(realRotaryPose, activeState.get().rotaryPose);
+        final double rotarayPIDDOutput = rotaryPoseController.calculate(realRotaryPose.getRadians(), activeState.get().rotaryPose.getRadians());
 
         final double requestedRotaryVolts = TorqueMath.constrain(rotarayPIDDOutput + rotaryFFOutput, ROTARY_MAX_VOLTS);
         SmartDashboard.putNumber("arm::requestedRotaryVolts", requestedRotaryVolts);
 
-        rotary.setVolts(requestedRotaryVolts);
-    }
-
-    @Log.BooleanBox
-    public final boolean isConflictingWithIndexer() {
-        return ARM_INTERFERE_MIN < realRotaryPose && realRotaryPose < ARM_INTERFERE_MAX;
-    }
-
-    @Log.BooleanBox
-    public final boolean wantsToConflictWithIndexer() {
-        return wantsHandoff;
+        rotary.setVolts(output ? requestedRotaryVolts : 0);
     }
 }
