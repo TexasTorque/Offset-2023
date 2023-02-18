0502bc3ff9574fd7ff6f134857b413b037d350f8
Justus Languell <jlanguell@trainbeyond.com>
2023-02-17 22:45:43 -0600

    Going to rework handoff

--- src/main/java/org/texastorque/auto/EventMap.java
+++ src/main/java/org/texastorque/auto/EventMap.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.texastorque.Subsystems;
-import org.texastorque.auto.routines.Pickup;
+import org.texastorque.auto.routines.Handoff;
 import org.texastorque.subsystems.Arm;
 import org.texastorque.subsystems.Hand;
 import org.texastorque.subsystems.Intake;
@@ -27,7 +27,7 @@ public final class EventMap implements Subsystems {
             hand.setState(Hand.State.OPEN);
         }));
 
-        map.put("pickup", new TorqueSequenceRunner(new Pickup()));
+        map.put("pickup", new TorqueSequenceRunner(new Handoff()));
 
         // Legacy
         map.put("arm-ready", new TorqueExecute(() -> arm.setState(Arm.State.TOP)));
--- src/main/java/org/texastorque/auto/routines/Handoff.java
+++ src/main/java/org/texastorque/auto/routines/Handoff.java
@@ -13,8 +13,9 @@ import org.texastorque.subsystems.Intake;
 import org.texastorque.torquelib.auto.TorqueSequence;
 import org.texastorque.torquelib.auto.commands.TorqueWaitForSeconds;
 
-public final class Pickup extends TorqueSequence implements Subsystems {
-    public Pickup() {
+public final class Handoff extends TorqueSequence implements Subsystems {
+    // This must be reworked!
+    public Handoff() {
         addBlock(arm.setStateCommand(Arm.State.HANDOFF));
         addBlock(new TorqueWaitForSeconds(.5));
         addBlock(hand.setStateCommand(Hand.State.CLOSE), arm.setStateCommand(Arm.State.BACK));
