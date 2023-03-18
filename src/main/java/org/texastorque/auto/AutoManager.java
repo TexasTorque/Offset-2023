f7f0a412bc11d91d48f053a84f272fd017cb66e8
Justus Languell <jlanguell@trainbeyond.com>
2023-03-18 15:44:13 -0500

    Meeting 3/18

--- src/main/java/org/texastorque/auto/AutoManager.java
+++ src/main/java/org/texastorque/auto/AutoManager.java
@@ -6,15 +6,8 @@
  */
 package org.texastorque.auto;
 
-
 import org.texastorque.auto.sequences.houston.OnePieceMobilityEngage;
 import org.texastorque.auto.sequences.houston.TwoPieceMobilityEngage;
-import org.texastorque.auto.sequences.waco.DumbBothSide1PieceMobility;
-import org.texastorque.auto.sequences.waco.DumbBumpySide1PieceEngage;
-import org.texastorque.auto.sequences.waco.DumbFlatSide1PieceEngage;
-import org.texastorque.auto.sequences.waco.DumbMiddleSide1PieceEngage;
-import org.texastorque.auto.sequences.waco.FlatSide2PieceEngage;
-import org.texastorque.auto.sequences.waco.FlatSide3Piece;
 import org.texastorque.torquelib.auto.TorqueAutoManager;
 
 public final class AutoManager extends TorqueAutoManager {
@@ -33,16 +26,15 @@ public final class AutoManager extends TorqueAutoManager {
     public final void init() {
         // addSequence(new BumpySide2PieceLevel());
         // addSequence(new BumpySide3Piece());
-        addSequence(new FlatSide2PieceEngage());
-        addSequence(new FlatSide3Piece());
+        // addSequence(new FlatSide2PieceEngage());
+        // addSequence(new FlatSide3Piece());
         // addSequence(new MiddleSide1PieceLevel());
-        addSequence(new DumbBothSide1PieceMobility());
-        addSequence(new DumbMiddleSide1PieceEngage());
-        addSequence(new DumbFlatSide1PieceEngage());
-        addSequence(new DumbBumpySide1PieceEngage());
+        // addSequence(new DumbBothSide1PieceMobility());
+        // addSequence(new DumbMiddleSide1PieceEngage());
+        // addSequence(new DumbFlatSide1PieceEngage());
+        // addSequence(new DumbBumpySide1PieceEngage());
 
         addSequence(new OnePieceMobilityEngage());
         addSequence(new TwoPieceMobilityEngage());
-
     }
 }
