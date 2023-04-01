e4a209e3205c3063defaf73c5ce06f4d2205a573
0marA <theengineermachine@gmail.com>
2023-04-01 12:59:18 -0500

    New offsets

--- src/main/java/org/texastorque/subsystems/Arm.java
+++ src/main/java/org/texastorque/subsystems/Arm.java
@@ -101,7 +101,7 @@ public final class Arm extends TorqueSubsystem implements Subsystems {
         }
     }
 
-    private static final double ROTARY_ENCODER_OFFSET = Units.degreesToRadians(79 - 264),
+    private static final double ROTARY_ENCODER_OFFSET = Units.degreesToRadians(-6),
             ELEVATOR_MAX_VOLTS_UP = 12,
             ELEVATOR_MAX_VOLTS_HANDOFF = 12,
             ROTARY_MAX_VOLTS = 12,
@@ -365,7 +365,7 @@ public final class Arm extends TorqueSubsystem implements Subsystems {
     }
 
     private void calculateRotary(final State state) {
-        double armSetpoint = state.get().rotaryPose.getRadians() ;//+ setpointAdjustment * Units.degreesToRadians(30);
+        double armSetpoint = state.get().rotaryPose.getRadians();// + setpointAdjustment * Units.degreesToRadians(30);
         double rotaryPos = realRotaryPose.getRadians();
         if (rotaryPos > Math.toRadians(315)) { // wrap around up to prevent overshoot causing a massive spin.
             rotaryPos = rotaryPos - 2 * Math.PI;
